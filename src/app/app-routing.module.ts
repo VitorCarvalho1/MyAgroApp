a6ebfac347a4f00f7b0118efc195f4a173e24ebe
VitorCarvalho1 <vitorhugo.carvalho2@yahoo.com.br>
2022-12-05 23:20:08 -0300

    add pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
-import { NgModule } from '@angular/core';
+import { UsersComponent } from './users/users.component';
+import { VisualizarComponent } from './visualizar/visualizar.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: '', component:CadastroComponent},
+  {path: 'visualizar', component: VisualizarComponent},
+  {path: 'users', component: UsersComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HttpClientModule } from '@angular/common/http';
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { VisualizarComponent } from './visualizar/visualizar.component';
+import { MenuFooterComponent } from './menu-footer/menu-footer.component';
+import { UsersComponent } from './users/users.component';
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CadastroComponent,
+    VisualizarComponent,
+    MenuFooterComponent,
+    UsersComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
